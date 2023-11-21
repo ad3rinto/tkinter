582c5b447888d59b0041dceaa01ae9180cb3a70b
ad3rinto <aad3rinto@gmail.com>
2023-11-21 14:37:01 +0000

    kwargs

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=400)
 
 # Creating a label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+label_text = "win"
+my_label = Label(text=label_text, font=("Arial", 24, "bold"))
 my_label.pack()
 
+# Entry component
+board_input = Entry(width=10)
+board_input.pack()
 
 
+def button_clicked():
+    new_text = board_input.get()
+    my_label["text"] = new_text
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
 
 window.mainloop()
