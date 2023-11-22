5a9cb39cfd1ac41c3d13c8f2df594e9b45a985cc
ad3rinto <aad3rinto@gmail.com>
2023-11-22 20:51:13 +0000

    built mile to Km converter

---
+++ lenght_converter.py
@@ -0,0 +1,33 @@
+import math
+from tkinter import *
+
+window = Tk()
+window.title("Mile to Km Converter")
+window.minsize(width=400, height=200)
+
+
+def calculate():
+    miles = int(user_input.get())
+    kilo = round(miles * 1.6, 2)
+    answer_label["text"] = kilo
+
+
+user_input = Entry(width=10, font=("Arial", 24))
+user_input.insert(END, "0")
+user_input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles", font=("Arial", 24))
+miles_label.grid(row=0, column=2)
+
+equal_label = Label(text="is equal to", font=("Arial", 24))
+equal_label.grid(row=1, column=0)
+
+answer_label = Label(text="0", font=("Arial", 24))
+answer_label.grid(row=1, column=1)
+
+km_label = Label(text="Km", font=("Arial", 24))
+km_label.grid(row=1, column=2)
+
+calculate_button = Button(text="Click Me", command=calculate)
+calculate_button.grid(row=2, column=1)
+window.mainloop()
